2ffbc4ef9c06407c97bf87ffbc81e0c94bd7f2c4
rri\riancu <robert.iancu@realpage.com>
2024-05-10 10:43:27 +0300

    Add main-shell component

---
+++ src/shell/feature/main-shell/main-shell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-shell',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-shell.component.html',
+  styleUrl: './main-shell.component.scss'
+})
+export class MainShellComponent {
+
+}
