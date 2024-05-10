33a8e0e8f93975415e99061fecdaee15c620388c
iancurobert92 <iancurobert92@gmail.com>
2024-05-10 21:19:36 +0300

    Create The Home Page (#3)
    * Add new global styles
    
    * Add home page content
    
    * Set fixed position for the toolbar
    
    * Add page-section component
    
    * Add import path for the home ui module
    
    * Use page-section component
    
    ---------
    
    Co-authored-by: rri\riancu <robert.iancu@realpage.com>

--- src/home/feature/home/home.component.ts
+++ src/home/feature/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PageSectionComponent } from '@home/ui';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PageSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
---
+++ src/home/ui/index.ts
@@ -0,0 +1,1 @@
+export * from './page-section/page-section.component';
