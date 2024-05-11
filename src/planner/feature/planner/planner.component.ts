d0ff3e9d2afed31e567762e913297a4ccb4cd6da
rri\riancu <robert.iancu@realpage.com>
2024-05-11 09:15:43 +0300

    Display coordinates-input and coordinates-table components

--- src/planner/feature/planner/planner.component.ts
+++ src/planner/feature/planner/planner.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CoordinatesInputComponent } from '../coordinates-input/coordinates-input.component';
+import { CoordinatesTableComponent } from '../coordinates-table/coordinates-table.component';
 
 @Component({
   selector: 'app-planner',
   standalone: true,
-  imports: [],
+  imports: [CoordinatesInputComponent, CoordinatesTableComponent],
   templateUrl: './planner.component.html',
-  styleUrl: './planner.component.scss'
+  styleUrl: './planner.component.scss',
 })
-export class PlannerComponent {
-
-}
+export class PlannerComponent {}
