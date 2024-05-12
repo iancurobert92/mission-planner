5c34c142c8a850ccbd907ae5292dc3db50ae2179
rri\riancu <robert.iancu@realpage.com>
2024-05-12 18:18:13 +0300

    Add "delete all" function

--- src/planner/feature/planner/planner.component.ts
+++ src/planner/feature/planner/planner.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { CoordinatesInputComponent } from '../coordinates-input/coordinates-input.component';
 import { CoordinatesTableComponent } from '../coordinates-table/coordinates-table.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { PlannerService } from '../../data-access/services';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-planner',
   standalone: true,
-  imports: [CoordinatesInputComponent, CoordinatesTableComponent],
+  imports: [
+    CoordinatesInputComponent,
+    CoordinatesTableComponent,
+    MatButton,
+    MatIcon,
+  ],
   templateUrl: './planner.component.html',
   styleUrl: './planner.component.scss',
 })
-export class PlannerComponent {}
+export class PlannerComponent {
+  private destroyRef = inject(DestroyRef);
+
+  constructor(private plannerService: PlannerService) {}
+
+  onDeleteButtonClick() {
+    this.plannerService
+      .deleteAllCoordinates()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe();
+  }
+}
