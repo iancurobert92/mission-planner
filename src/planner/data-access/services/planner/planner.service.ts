81f383a38be5c3c859aa8950b704a4b13ea12bee
rri\riancu <robert.iancu@realpage.com>
2024-05-12 18:16:26 +0300

    Update planner service

--- src/planner/data-access/services/planner/planner.service.ts
+++ src/planner/data-access/services/planner/planner.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ArrayUtils } from '@planner/util';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Coordinate } from '../../models';
+import { Coordinate, MissionService } from '@shared/data-access';
+import { BehaviorSubject, EMPTY, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,13 +13,33 @@ export class PlannerService {
     return this.coordinatesSubj.asObservable();
   }
 
+  constructor(private missionService: MissionService) {}
+
+  getCoordinates() {
+    return this.missionService
+      .getMissionTargetPoints()
+      .pipe(tap((value) => this.coordinatesSubj.next(value)));
+  }
+
   addCoordinate(value: Coordinate) {
-    if (ArrayUtils.hasElement(value, this.coordinatesSubj.value, ['name']))
-      return alert(`The entry with the name "${value.name}" already exists.`);
+    if (ArrayUtils.hasElement(value, this.coordinatesSubj.value, ['name'])) {
+      alert(`The entry with the name "${value.name}" already exists.`);
+      return EMPTY;
+    }
 
-    if (ArrayUtils.hasElement(value, this.coordinatesSubj.value, ['x', 'y']))
-      return alert('These coordonates already exists');
+    if (ArrayUtils.hasElement(value, this.coordinatesSubj.value, ['x', 'y'])) {
+      alert('These coordonates already exists');
+      return EMPTY;
+    }
 
-    this.coordinatesSubj.next([...this.coordinatesSubj.value, value]);
+    return this.missionService
+      .addMissionTargetPoint(value)
+      .pipe(tap((value) => this.coordinatesSubj.next(value)));
+  }
+
+  deleteAllCoordinates() {
+    return this.missionService
+      .deleteAllMissionTargetPoints()
+      .pipe(tap((value) => this.coordinatesSubj.next(value)));
   }
 }
